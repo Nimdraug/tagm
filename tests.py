b61bf8e3260b95f7e0f258a7ccccab708d7d602d
Martin Hulten-Ashauer <martin@mooball.net>
2014-01-29 09:44:38 +1100

    Converted get obj by tags tests

--- tests.py
+++ tests.py
@@ -22,35 +22,36 @@ class TestAdd( TagmTestCase ):
     def test_add_subtag( self ):
         self.assertIsNone( self.db.add( [ [ 'a', 'b' ] ], [ 'obj1' ] ) )
 
-def test_get_objs_by_tags():
-    test_add_tags()
-    
-    res = db.get( [ 'a', 'b' ] )
-    assert res == [ 'obj1', 'obj2' ]
-    
-    res = db.get( [ 'a', 'd' ] )
-    assert res == [ 'obj1' ]
+class TagmGetTestCase( TagmTestCase ):
+    def setUp( self ):
+        super( TagmGetTestCase, self ).setUp()
+        
+        self.db.add( [ 'a' ], [ 'obj1', 'obj2', 'obj3' ] )
+        self.db.add( [ 'b' ], [ 'obj2', 'obj3' ] )
+        self.db.add( [ 'c' ], [ 'obj3' ] )
+        self.db.add( [ [ 'c', 'd' ] ], [ 'obj1' ] )
 
-    res = db.get( [ 'a', 'f' ] )
-    assert res == [ 'obj2' ]
+class TestGetObjsByTags( TagmGetTestCase ):
+    def test_get_single_tag( self ):
+        self.assertEqual( self.db.get( [ 'b' ] ), [ 'obj2', 'obj3' ] )
 
-    res = db.get( [ 'd', 'f' ] )
-    assert res == []
+    def test_get_multiple_tags( self ):
+        self.assertEqual( self.db.get( [ 'a', 'b' ] ), [ 'obj2', 'obj3' ] )
     
-    res = db.get( [ 'a', 'i' ], subtags = True )
-    assert res == [ 'obj1', 'obj2' ]
+    def test_get_subtag( self ):
+        self.assertEqual( self.db.get( [ [ 'c', 'd' ] ] ), [ 'obj1' ] )
     
-    res = db.get( [ 'a', [ 'i', 'j' ] ] )
-    assert res == [ 'obj1' ]
+    def test_get_subtag_parent( self ):
+        self.assertEqual( self.db.get( [ [ 'c' ] ] ), [ 'obj3' ] )
     
-    res = db.get( [] )
-    assert res == [ 'obj1', 'obj2' ]
+    def test_get_subtag_parent_include_subtags( self ):
+        self.assertEqual( self.db.get( [ 'c' ], subtags = True ), [ 'obj3', 'obj1' ] )
     
-    # TODO: Should this raise an exception? Ie TagNotFoundError
-    #       Requires TagmDB.get to not handle TagNotFoundError
-    #       Question is, is that desired behavior?
-    res = db.get( [ 'h' ] )
-    assert res == []
+    def test_get_invalid_tag( self ):
+        # TODO: Should this raise an exception? Ie TagNotFoundError
+        #       Requires TagmDB.get to not handle TagNotFoundError
+        #       Question is, is that desired behavior?
+        self.assertEqual( self.db.get( [ 'e' ] ), [] )
     
 def test_get_tags_by_tags():
     test_add_tags()
